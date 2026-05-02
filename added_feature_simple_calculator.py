a92a4b0f62d5971bd5d5271419f093755d0b493f
yanab <yanaborja610@gmail.com>
2026-05-02 22:50:48 +0800

    Added method to show history with entry and appending the last result

--- added_feature_simple_calculator.py
+++ added_feature_simple_calculator.py
@@ -2,5 +2,11 @@ class AddedFeature(BasicCalculator):
     def __init__(self, filename ="calc_history.txt"):
         self.filename = filename
         self.history = []
+        self.last_result = 0
 
+    def show_history(self, expression, result):
+        entry = {"expression": expression, "result": result}
+
+        self.history.append(entry)
+        self.last_result = result
 

f54b0313185a0dfd12075bd82451d71ec046f2c2
yanab <yanaborja610@gmail.com>
2026-05-02 23:32:20 +0800

    Importing the class on my main program and added the method of displaying the main menu

---
+++ main_program_simple_calculator.py
@@ -0,0 +1,10 @@
+from added_feature_simple_calculator import AddedFeature
+
+def main_calculator():
+    calculator = AddedFeature()
+
+    print("Simple App Calculator")
+    print("Commands: undo | history | exit")
+    print("You can also type expression like: 5+3 and another +2 (for chaining)\n")
+
+    

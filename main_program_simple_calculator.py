ef8eeae69290cc6c8410b01ee30788a78665f991
yanab <yanaborja610@gmail.com>
2026-05-02 23:36:08 +0800

    Used while loop to ask user input and to run the options such as undo and show history

--- main_program_simple_calculator.py
+++ main_program_simple_calculator.py
@@ -7,4 +7,15 @@ def main_calculator():
     print("Commands: undo | history | exit")
     print("You can also type expression like: 5+3 and another +2 (for chaining)\n")
 
-    
+    while True:
+        user_input = input("Enter expression: ").strip()
+
+        if user_input.lower() == "exit":
+            print("Exiting...")
+            break
+
+        elif user_input.lower() == "history":
+            calculator.show_history()
+
+        elif user_input.lower() == "undo":
+            calculator.undo()
